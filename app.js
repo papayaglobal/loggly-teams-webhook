664bdd008370dabe08a3654a5722cd42989ce3ab
ofer <ofer@papayaglobal.com>
2020-04-22 15:16:53 +0300

    functional version

--- app.js
+++ app.js
@@ -1,58 +1,116 @@
 const ApiBuilder = require("claudia-api-builder");
 const get = require("lodash.get");
+const https = require("https");
+const URL = require("url").URL;
+const jsesc = require("jsesc");
 
 const api = new ApiBuilder();
 module.exports = api;
+
+const LOG_LEVEL_IMAGES = {
+  error:
+    "https://github.com/papayaglobal/loggly-teams-webhook/raw/master/images/error.png",
+  warn:
+    "https://github.com/papayaglobal/loggly-teams-webhook/raw/master/images/warn.png",
+  info:
+    "https://github.com/papayaglobal/loggly-teams-webhook/raw/master/images/info.png",
+  log:
+    "https://github.com/papayaglobal/loggly-teams-webhook/raw/master/images/question.png",
+};
+const stringifyEscaped = data => {
+  return jsesc(JSON.stringify(data, null, 2), {
+    quotes: "double",
+  }).replace(/\\[nr]/g, "\\n\\n")
+};
 
 const createTeamsMsg = (body) => {
   const summary = get(body, "alert_name");
   const logText = get(body, "recent_hits[0]");
   const log = !!logText ? JSON.parse(logText) : {};
-  const title = get(log, "msg") | "";
-  const logLevel = get(log, "level") | "log";
+  const title = get(log, "msg") || "";
+  const logLevel = get(log, "level") || "log";
   const searchLink = get(body, "search_link");
-  const snoozeLink = get(body,"snooze_link");
+  const snoozeLink = get(body, "snooze_link");
+  const image = LOG_LEVEL_IMAGES[logLevel] || LOG_LEVEL_IMAGES.log;
 
   return `
   {
-    "contentType": "application/vnd.microsoft.teams.card.o365connector",
-    "content": {
-      "@type": "MessageCard",
-      "@context": "http://schema.org/extensions",
-      "summary": "${summary}",
-      "sections": [
+    "@type": "MessageCard",
+    "@context": "http://schema.org/extensions",
+    "summary": "${summary}",
+    "sections": [
+      {
+        "activityTitle": "${summary}",
+        "activitySubtitle": "${stringifyEscaped(title)}",
+        "activityImage": "${image}",
+        "text": "${stringifyEscaped(log)}"
+      }
+    ],
+    "potentialAction": [
+      {
+        "@type": "OpenUri",
+        "name": "View in loggly",
+        "targets": [
           {
-              "activityTitle": "${title}",
-              "activitySubtitle": "${logLevel}",
-              "activityText": "${JSON.stringify(log, null, 2)}",
-              "activityImage": "http://connectorsdemo.azurewebsites.net/images/MSC12_Oscar_002.jpg"
+            "os": "default",
+            "uri": "${searchLink}"
           }
-      ],
-      "potentialAction": [
+        ]
+      },
+      {
+        "@type": "HttpPOST",
+        "name": "Snooze",
+        "targets": [
           {
-              "@context": "http://schema.org",
-              "@type": "OpenUri",
-              "name": "View in loggly",
-              "target": [
-                  "${searchLink}"
-              ]
-          },
-          {
-            "@context": "http://schema.org",
-            "@type": "HttpPOST",
-            "name": "Snooze",
-            "target": [
-                "${snoozeLink}"
-            ]
-        }
-      ]
-    }
+            "os": "default",
+            "uri": "${snoozeLink}"
+          }
+        ]
+      }
+    ]
   }
   `;
 };
 
+const sendMsg = (msg, webhookURL) => {
+  return new Promise((resolve, reject) => {
+    const url = new URL(webhookURL);
+    const post_options = {
+      host: url.host,
+      path: url.pathname,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(msg),
+      },
+    };
+
+    console.log("sending requst", post_options, msg);
+    const post = https.request(post_options, (res) => {
+      console.log("statusCode:", res.statusCode);
+      res.on("data", (d) => {
+        console.log(d.toString());
+        resolve();
+      });
+    });
+    post.on("error", (e) => {
+      console.log("Error sending requst to teams", e);
+      reject(e);
+    });
+    post.write(msg);
+    post.end(() => {
+      console.log("request sent");
+    });
+  });
+};
+
 api.post("/loggly", function (request) {
-  const msg = createTeamsMsg(request.body);
+  console.log("start", request.body);
+  return sendMsg(createTeamsMsg(request.body), request.env["WEBHOOK_URL"]);
 });
 
-api.addPostDeployConfig("webhook URL", "Enter a message:", "teams-webhook-url");
+api.addPostDeployConfig(
+  "WEBHOOK_URL",
+  "Enter the webhook URL:",
+  "teams-webhook-url"
+);
--- test.js
+++
@@ -1,9 +0,0 @@
-import ApiBuilder from 'claudia-api-builder';
-import get from 'lodash.get';
-
-const body = { alert_name: 'werwer'};
-
-const summary = get(body, 'alert_name');
-
-
-console.log({summary});
